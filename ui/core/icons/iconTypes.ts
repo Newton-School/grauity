0d2efb326e03e7d4e889187b68b2c38b49011d39
Praduman Goyal <goyal.praduman611@gmail.com>
2023-01-03 23:06:50 +0530

    Add auto generated icon types

---
+++ ui/core/icons/iconTypes.ts
@@ -0,0 +1,10 @@
+export type guiIconName = "school" | "code" | "today" | "person";
+
+export type guiIconKey = "School" | "Code" | "Today" | "Person";
+
+export enum GUI_ICONS {
+    School = 'school',
+    Code = 'code',
+    Today = 'today',
+    Person = 'person',
+}
