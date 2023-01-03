d79dc7eea1b6f75cf933db2848a6598a44a657ad
Praduman Goyal <goyal.praduman611@gmail.com>
2023-01-03 23:32:33 +0530

    Add prettifier and eslint to the typescript files

--- ui/core/icons/iconTypes.ts
+++ ui/core/icons/iconTypes.ts
@@ -1,6 +1,6 @@
-export type guiIconName = "school" | "code" | "today" | "person";
+export type guiIconName = 'school' | 'code' | 'today' | 'person';
 
-export type guiIconKey = "School" | "Code" | "Today" | "Person";
+export type guiIconKey = 'School' | 'Code' | 'Today' | 'Person';
 
 export enum GUI_ICONS {
     School = 'school',
