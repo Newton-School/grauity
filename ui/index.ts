fef6ced6f918d29cad24adf4a9157afa8daa75e9
Praduman Goyal <goyal.praduman611@gmail.com>
2023-01-05 11:33:38 +0530

    Add preset-react loader

--- ui/index.ts
+++ ui/index.ts
@@ -3,6 +3,5 @@ import { GUI_ICONS as guiIcons, TAG_ICONS } from './core';
 
 export { guiIcons, TAG_ICONS };
 
-export class Api {
-    // ...
-}
+export type { IconProps } from './elements';
+export { Icon } from './elements';
--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,7 @@ module.exports = {
                         presets: [
                             '@babel/preset-env',
                             '@babel/preset-typescript',
+                            '@babel/preset-react',
                         ],
                     },
                 },
