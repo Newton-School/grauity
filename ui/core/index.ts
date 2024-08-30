d6ecd3915183ee846f4a5daff581bc874ad6db60
Mihir Sachdeva <mihirsachdeva.ms@gmail.com>
2024-08-30 15:25:07 +0530

    Add updates

--- ui/core/index.ts
+++ ui/core/index.ts
@@ -1,17 +1,14 @@
-export type { grauityIconSizeName, grauitySizeName } from './sizes';
-export { GRAUITY_ICON_SIZES, GRAUITY_SIZE } from './sizes';
-
-export type { grauityIconColorName, grauityColorName } from './colors';
-export { GRAUITY_ICON_COLORS, GRAUITY_COLOR } from './colors';
-
-export type { grauityIconName, grauityIconKey } from './icons';
+export type { grauityColorName, grauityIconColorName } from './colors';
+export { GRAUITY_COLOR, GRAUITY_ICON_COLORS } from './colors';
+export type { grauityIconKey, grauityIconName } from './icons';
 export { GRAUITY_ICONS, ICON_TAGS, TAG_ICONS } from './icons';
-
 export type {
-    grauityRotatedChoiceName,
     grauityFlippedChoiceName,
+    grauityRotatedChoiceName,
 } from './miscellaneous-choices';
 export {
-    GRAUITY_ICON_ROTATED_CHOICES,
     GRAUITY_ICON_FLIPPED_CHOICES,
+    GRAUITY_ICON_ROTATED_CHOICES,
 } from './miscellaneous-choices';
+export type { grauityIconSizeName, grauitySizeName } from './sizes';
+export { GRAUITY_ICON_SIZES, GRAUITY_SIZE } from './sizes';
