4db1ab4f6dc03ba171a2442f720d682a797329ad
Mihir Sachdeva <mihirsachdeva.ms@gmail.com>
2024-09-06 17:26:11 +0530

    Add storybook a11y addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
     stories: [
         '../stories/**/*.mdx',
         '../stories/**/*.stories.@(js|jsx|ts|tsx)',
@@ -8,9 +8,9 @@ module.exports = {
         '@storybook/addon-links',
         '@storybook/addon-essentials',
         '@storybook/addon-interactions',
-        // '@storybook/addon-mdx-gfm',
         '@storybook/addon-webpack5-compiler-babel',
         '@storybook/addon-docs',
+        '@storybook/addon-a11y',
     ],
 
     framework: {
