0e12c047e14bc87d4b0e0f48ce4f7bab251ca648
Saksham Bindal <saksham.bindal2004@gmail.com>
2024-09-24 15:58:55 +0530

    props documentation

--- ui/elements/PopOver/types.ts
+++ ui/elements/PopOver/types.ts
@@ -10,14 +10,56 @@ export type PopOverOffset = {
 };
 
 export interface PopOverProps {
+    /**
+     * Whether the PopOver is open or not.
+     * @default false
+     */
     isOpen?: boolean;
+
+    /**
+     * Ref to the trigger element. Required to calculate the position of the PopOver.
+     * @default null
+     */
     triggerRef?: React.RefObject<HTMLDivElement>;
+
+    /**
+     * Direction in which the PopOver should open.
+     * @default bottom
+     */
     direction?: PopOverDirection;
+
+    /**
+     * Content to be displayed inside the PopOver.
+     */
     children?: React.ReactNode;
+
+    /**
+     * Flag to enable or disable auto adjustment of the PopOver position.
+     * @default true
+     */
     autoAdjust?: boolean;
+
+    /**
+     * Ref to the parent element. Required to calculate the position of the PopOver.
+     * @default document.body
+     */
     parentRef?: React.RefObject<HTMLDivElement>;
+
+    /**
+     * Minimum margin offset to be maintained from the parent container.
+     * @default { top: 0, left: 0, right: 0, bottom: 0 }
+     */
     minimumOffset?: PopOverOffset;
+
+    /**
+     * Flag to enable or disable closing the PopOver on outside click.
+     * @default true
+     */
     shouldCloseOnOutsideClick?: boolean;
+
+    /**
+     * Callback function triggered when the PopOver is closed.
+     */
     onClose?: () => void;
 }
 
