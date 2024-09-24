36760b8bfe4e814200fcb2bcb8756d3df2630d29
Shubhaam Tiwary <89575981+ShubhaamTiwary@users.noreply.github.com>
2024-09-24 17:35:12 +0530

    NS-8383: Add Chip Component (#21)
    * add chip component
    
    * add test in chip component
    
    * Remove unused ChipFontSize interface
    
    * remove decorators
    
    * Refactor ChipProps interface in types.ts
    
    * add gallery for chip component
    
    * refactor

---
+++ stories/elements/Chip/Gallery.stories.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import Chip, { ChipProps } from 'ui/elements/Chip';
+import { CHIP_VARIANTS } from 'ui/elements/Chip/constants';
+import Table from 'ui/elements/Table';
+
+import TokenBlock from '../../helper-components/TokenBlock';
+
+export default {
+    title: 'Elements/Chip',
+    component: Chip,
+    tags: ['!autodocs'],
+};
+
+const Template = (args: ChipProps) => (
+    <Table.Table borderAround={false} borderVertical={false}>
+        <Table.TableHead highlightHeaders={false}>
+            <Table.TableRow>
+                <Table.TableHeadingCell align="left">
+                    Chip variant
+                </Table.TableHeadingCell>
+                <Table.TableHeadingCell align="left">
+                    Chip
+                </Table.TableHeadingCell>
+            </Table.TableRow>
+        </Table.TableHead>
+        <Table.TableBody>
+            {CHIP_VARIANTS.map((variant) => (
+                <Table.TableRow>
+                    <Table.TableDataCell>
+                        <TokenBlock copy>{variant}</TokenBlock>
+                    </Table.TableDataCell>
+                    <Table.TableDataCell>
+                        <Chip {...args} variant={variant} key={variant}>
+                            Chip : 01
+                        </Chip>
+                    </Table.TableDataCell>
+                </Table.TableRow>
+            ))}
+        </Table.TableBody>
+    </Table.Table>
+);
+
+const defaultArgs: ChipProps = {
+    variant: 'brand',
+    size: 'medium',
+    hasBorder: false,
+    textColor: null,
+    backgroundColor: null,
+    borderColor: null,
+};
+
+export const Gallery = Template.bind({});
+
+Gallery.args = {
+    ...defaultArgs,
+};
---
+++ stories/elements/Chip/index.stories.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Chip from 'ui/elements/Chip/Chip';
+import { ChipProps } from 'ui/elements/Chip/types';
+
+export default {
+    title: 'Elements/Chip',
+    component: Chip,
+};
+
+const Template = (args: ChipProps) => <Chip {...args}>Chip : 01</Chip>;
+
+const defaultArgs: ChipProps = {
+    variant: 'brand',
+    size: 'medium',
+    hasBorder: false,
+    textColor: null,
+    backgroundColor: null,
+    borderColor: null,
+};
+
+export const Component = Template.bind({});
+
+Component.args = {
+    ...defaultArgs,
+};
