b56f48fac406a3fc975a2c320c29bd49f18f5837
Saksham Bindal <saksham.bindal2004@gmail.com>
2024-09-24 17:39:49 +0530

    NS-8381: Add PopOver Component (#22)
    * non adjusted popover component
    
    * auto adjusting popover position
    
    * trigger outside container
    
    * auto adjusting fixed
    
    * isopen prop
    
    * cleanup
    
    * isopen bug fixed
    
    * close on outside click
    
    * minimum offset fixed
    
    * infinite update fixed
    
    * popover story decorators
    
    * props documentation
    
    * unit test cases
    
    * any in trigger and parent ref
    
    * fix click away when triggerRef not given
    
    * grauity docs
    
    * disable background scroll
    
    * docs

--- hooks/useDisableBodyScroll.ts
+++ hooks/useDisableBodyScroll.ts
@@ -5,21 +5,25 @@ import { useEffect } from 'react';
  * Hook that makes the body scrollable or not scrollable.
  * Useful for modals, dialogs, etc.
  */
-const useDisableBodyScroll = () => {
+const useDisableBodyScroll = (disableBodyScroll: boolean = true) => {
     useEffect(() => {
-        if (typeof window !== 'undefined' && document?.body) {
-            // TODO: This is a fix for the sudden content shift towards the right
-            // which happens when the body is set to overflow hidden (scrollbars are removed).
-            // But, normally we do not show the scrollbar in `newton-web`.
-            // document.body.style.paddingRight = '15px';
+        if (disableBodyScroll) {
+            if (typeof window !== 'undefined' && document?.body) {
+                // TODO: This is a fix for the sudden content shift towards the right
+                // which happens when the body is set to overflow hidden (scrollbars are removed).
+                // But, normally we do not show the scrollbar in `newton-web`.
+                // document.body.style.paddingRight = '15px';
 
-            document.body.style.overflow = 'hidden';
+                document.body.style.overflow = 'hidden';
+            }
         }
 
         return () => {
-            if (typeof window !== 'undefined' && document?.body) {
-                // document.body.style.paddingRight = 'unset';
-                document.body.style.overflow = 'auto';
+            if (disableBodyScroll) {
+                if (typeof window !== 'undefined' && document?.body) {
+                    // document.body.style.paddingRight = 'unset';
+                    document.body.style.overflow = 'auto';
+                }
             }
         };
     }, []);
---
+++ ui/elements/PopOver/index.tsx
@@ -0,0 +1,2 @@
+export { default } from './PopOver';
+export type { PopOverProps } from './types';
