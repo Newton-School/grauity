c5d58c0af9d293cb9fe1f72c29c4ba90591c626c
Saksham Bindal <saksham.bindal2004@gmail.com>
2024-09-25 15:45:44 +0530

    prop documentation

--- ui/elements/Calendar/CalendarEvent/types.ts
+++ ui/elements/Calendar/CalendarEvent/types.ts
@@ -1,17 +1,70 @@
 import React from 'react';
 
 export interface CalendarEventProps {
+    /**
+     * The title of the calendar event.
+     */
     title: string;
+
+    /**
+     * Optional content to be displayed on a chip within the event.
+     * If not provided, the chip will not be displayed.
+     */
     chipContent?: string;
+
+    /**
+     * The start date and time of the event.
+     */
     start: Date;
+
+    /**
+     * The end date and time of the event.
+     */
     end: Date;
+
+    /**
+     * Optional click handler for the event.
+     */
     onClick?: () => void;
+
+    /**
+     * Indicates whether the event is focused.
+     */
     focused?: boolean;
+
+    /**
+     * Minimum duration (in milliseconds) required to display the event time.
+     */
     minDurationToDisplayTime?: number;
+
+    /**
+     * Text color of the event.
+     * @default 'var(--text-action2)'
+     */
     textColor?: string;
+
+    /**
+     * Background color of the event.
+     * @default 'var(--bg-action-brand)'
+     */
     backgroundColor?: string;
+
+    /**
+     * Border color of the event.
+     * @default 'var(--border)'
+     */
     borderColor?: string;
+
+    /**
+     * Text color of the chip content.
+     * @default 'var(--text-action2)'
+     */
     chipTextColor?: string;
+
+    /**
+     * Background color of the chip content.
+     * @default 'var(--color-brand-600)'
+     */
     chipBackgroundColor?: string;
 }
 
