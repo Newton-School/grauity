ab786ecd61a661d959b511f169b4a325d97f93d1
Saksham Bindal <saksham.bindal2004@gmail.com>
2024-09-26 18:41:37 +0530

    Add Weekly Calendar Component (#20)
    * basic calendar layout
    
    * calendar container
    
    * change week in the calendar
    
    * color fix
    
    * week control from props
    
    * aria labels in week controller
    
    * aria label in scrollable timeline
    
    * scrollable calendar in both directions
    
    * show current time stick
    
    * highlight current day timeline
    
    * z-index fix
    
    * initial render from 8.30 am
    
    * example for calendar without week controls
    
    * border fixes
    
    * show events in calendar
    
    * overlapping events
    
    * more events
    
    * event renderer
    
    * props documentation
    
    * scroll behaviolur fixed
    
    * test cases
    
    * prop refactor
    
    * prop refactor in without week control story
    
    * constants
    
    * font family correct
    
    * font family correct
    
    * Add Calendar Event Component (#27)
    
    * calendar event configuration
    
    * simple calendar event render in brand color
    
    * customize color props
    
    * display time only on minimum duration
    
    * cursor pointer
    
    * calendar event with popover
    
    * prop documentation
    
    * aria config
    
    * unit test cases
    
    * fix bg-action-error
    
    * remove calendar event with popover story
    
    * build
    
    * Integrate Events in Calendar Layout (#31)
    
    * default event renderer in calendar
    
    * styles for small and extra small events
    
    * dynamic events to show on current week
    
    * calendar event style fix
    
    * overlapped events correctly shown
    
    * dummy events
    
    * aria improve
    
    * trim event height to bottom of calendar
    
    * events dummy data
    
    * calendar event typography enhanced
    
    * remove text for 12 am
    
    * dummy events
    
    * events refactoring enhanced by useMemo and async process
    
    * test fix
    
    * storybook docs
    
    * bottomsheet cursor fix
    
    * bottom scheet closing bug fixed
    
    * fix usememo
    
    * docs

---
+++ ui/elements/Calendar/types.ts
@@ -0,0 +1,84 @@
+import React from 'react';
+
+export type EventRendererFn<T> = (event: CalendarEvent<T>) => React.ReactNode;
+
+export interface CalendarEvent<T = {}> {
+    title?: string;
+    start: Date;
+    end: Date;
+    allDay?: boolean;
+    render?: EventRendererFn<T>;
+}
+
+export type CalendarEventRecord<T> = Record<number, CalendarEvent<T>[]>;
+
+export type CalendarEventExtended<T> = CalendarEvent<T> & {
+    overlap: number;
+    index: number;
+    forcedEventData?: {
+        start: Date;
+        end: Date;
+    };
+};
+
+export type CalendarEventRecordExtended<T> = Record<
+    number,
+    CalendarEventExtended<T>[]
+>;
+
+export interface WeeklyCalendarProps<T = {}> {
+    /**
+     * An optional array of calendar events.
+     * @default []
+     */
+    events?: CalendarEvent<T>[];
+
+    /**
+     * A function to render each event.
+     */
+    eventRenderer?: EventRendererFn<T>;
+
+    /**
+     * A boolean indicating whether to show week controls.
+     * @default true
+     */
+    shouldShowWeekControls?: boolean;
+
+    /**
+     * The offset for the week relative to current week, where 0 represents the current week.
+     * @default 0
+     */
+    weekOffset?: number;
+
+    /**
+     * A callback function that is called when the week changes.
+     * @param weekOffset - The new week offset.
+     */
+    onWeekChange?: (weekOffset: number) => void;
+}
+
+export interface EventRendererProps<T = {}> {
+    event?: CalendarEvent;
+    eventRenderer?: EventRendererFn<T>;
+}
+
+export interface StyledDivProps extends React.HTMLAttributes<HTMLDivElement> {
+    ref?: React.Ref<HTMLDivElement>;
+}
+
+export interface StyledCalendarBlockProps extends StyledDivProps {
+    $active?: boolean;
+    $currentTimeStick?: number;
+}
+
+export interface StyledCalendarTimelineBlockProps extends StyledDivProps {
+    $text?: string;
+    $headerBlock?: boolean;
+}
+
+export interface StyledEventWrapperProps extends StyledDivProps {
+    $startPosition: number;
+    $height: number;
+    $widthFactor: number;
+    $indexFactor: number;
+}
