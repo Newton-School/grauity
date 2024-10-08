677ef5acad66fac35518f83f3eb996d4787a84d0
Saksham Bindal <saksham.bindal2004@gmail.com>
2024-10-08 15:54:45 +0530

    popover used in multi select dropdown

--- ui/elements/MultiSelectDropdown/MultiSelectDropdown.styles.ts
+++ ui/elements/MultiSelectDropdown/MultiSelectDropdown.styles.ts
@@ -47,9 +47,6 @@ export const StyledDropdownHeaderTitle = styled.div`
 
 export const StyledDropdownContainer = styled.div<StyledDivProps>`
     box-sizing: border-box;
-    position: absolute;
-    top: calc(100% + var(--spacing-4px, 4px));
-    width: 100%;
     max-height: 300px;
     padding: var(--spacing-8px, 8px);
     display: flex;
