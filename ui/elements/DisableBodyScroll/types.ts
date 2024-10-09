44021798e225f2dbcf2e14da05173118dbb314ce
Saksham Bindal <saksham.bindal2004@gmail.com>
2024-10-09 14:55:59 +0530

    prop docstring

--- ui/elements/DisableBodyScroll/types.ts
+++ ui/elements/DisableBodyScroll/types.ts
@@ -3,7 +3,15 @@ import React from 'react';
 import { StyledDivProps } from '../Calendar/types';
 
 export interface DisableBodyScrollProps {
+    /**
+     * Content to be displayed inside the DisableBodyScroll.
+     */
     children?: React.ReactNode;
+
+    /**
+     * Flag to determine if the body scroll should be disabled.
+     * @default true
+     */
     enabled?: boolean;
 }
 
