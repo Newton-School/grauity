841ea116fc79ed0be4d04d64c9b5dc6a9c11475a
Saksham Bindal <saksham.bindal2004@gmail.com>
2024-10-23 12:03:31 +0530

    docstring

--- ui/elements/RangeInput/types.ts
+++ ui/elements/RangeInput/types.ts
@@ -7,11 +7,40 @@ export type RangeInputValue = {
 };
 
 export interface RangeInputProps {
+    /**
+     * The minimum value of the range input
+     * @default 0
+     */
     minValue?: number;
+
+    /**
+     * The maximum value of the range input
+     * @default 100
+     */
     maxValue?: number;
+
+    /**
+     * The jump step value of the range input
+     * @default 1
+     */
     step?: number;
+
+    /**
+     * The current value of the range input
+     * @default { min: 0, max: 100 }
+     */
     value?: RangeInputValue;
+
+    /**
+     * The function to be called when the range input value changes
+     * @param value - The new value of the range input
+     */
     onChange?: (value: RangeInputValue) => void;
+
+    /**
+     * The width of the range input container
+     * @default '100%'
+     */
     width?: string;
 }
 
