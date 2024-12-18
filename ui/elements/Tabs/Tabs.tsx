884d29a3d0f5d6b6c43c8c3492f0f0f54a0a4b2b
himanshuc11 <80830461+himanshuc11@users.noreply.github.com>
2024-12-18 18:32:11 +0530

    Add Tab Compoment (#103)
    Co-authored-by: Saksham Bindal <saksham.bindal2004@gmail.com>

---
+++ ui/elements/Tabs/Tabs.tsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from 'react';
+
+import {
+    StyledTabContainer,
+    StyledTabItemContainer,
+    StyledTabItemText,
+} from './Tabs.styles';
+import { TabProps } from './types';
+
+function Tabs(props: TabProps) {
+    const {
+        tabItems = [],
+        backgroundColor = null,
+        onTabFocusChange = () => {},
+        initialActiveTab = 0,
+        focusBackgroundColor = null,
+        focusColor = null,
+        color = null,
+    } = props;
+
+    const [activeTab, setActiveTab] = useState(initialActiveTab);
+
+    const handleItemClick = (activeTabIndex: number) => {
+        setActiveTab(activeTabIndex);
+    };
+
+    useEffect(() => {
+        onTabFocusChange(activeTab);
+    }, [activeTab]);
+
+    return (
+        <StyledTabContainer backgroundColor={backgroundColor}>
+            {tabItems?.map((item, idx) => (
+                <StyledTabItemContainer
+                    onClick={() => handleItemClick(idx)}
+                    isActive={activeTab === idx}
+                    color={color}
+                    focusBackgroundColor={focusBackgroundColor}
+                    focusColor={focusColor}
+                >
+                    {typeof item === 'string' ? (
+                        <StyledTabItemText>{item}</StyledTabItemText>
+                    ) : (
+                        item
+                    )}
+                </StyledTabItemContainer>
+            ))}
+        </StyledTabContainer>
+    );
+}
+
+export default Tabs;
