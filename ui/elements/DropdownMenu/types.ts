dc5ba91aef436fe102b808bb471e00a46b96372e
Saksham Bindal <saksham.bindal2004@gmail.com>
2025-02-07 12:12:47 +0530

    create base interface

--- ui/elements/DropdownMenu/types.ts
+++ ui/elements/DropdownMenu/types.ts
@@ -3,7 +3,6 @@ import { grauityIconName } from 'ui/core';
 
 enum BaseItemType {
     SUB_HEADER = 'subheader',
-    SEARCH = 'search',
     DIVIDER = 'divider',
     DEFAULT = 'default',
     CHECKBOX = 'checkbox',
@@ -12,12 +11,6 @@ enum BaseItemType {
 type BaseItemSubHeaderProps = {
     type: BaseItemType.SUB_HEADER;
     title: string;
-};
-type BaseItemSearchProps = {
-    type: BaseItemType.SEARCH;
-    icon?: grauityIconName;
-    placeholder?: string;
-    onChange?: (value: string) => void;
 };
 type BaseItemDividerProps = {
     type: BaseItemType.DIVIDER;
@@ -28,9 +21,7 @@ type BaseItemDefaultProps = {
     description?: string;
     leftIcon?: grauityIconName;
     rightIcon?: grauityIconName;
-    checked?: boolean;
     disabled?: boolean;
-    onClick?: () => void;
 };
 type BaseItemCheckboxProps = {
     type: BaseItemType.CHECKBOX;
@@ -44,25 +35,126 @@ type BaseItemCheckboxProps = {
 
 type BaseItemProps =
     | BaseItemSubHeaderProps
-    | BaseItemSearchProps
     | BaseItemDividerProps
     | BaseItemDefaultProps
     | BaseItemCheckboxProps;
 
 export interface DropdownMenuProps {
+    /**
+     * Whether to show the header of the dropdown menu.
+     * - If `showHeader` is false, `title`, `overline`, and `subtext` will be ignored.
+     * - if `title`, `overline`, and `subtext` all are not provided, `showHeader` will be ignored.
+     * @default true
+     */
     showHeader?: boolean;
+
+    /**
+     * The title of the dropdown menu.
+     * @default 'Select'
+     */
     title?: string;
+
+    /**
+     * The overline text of the dropdown menu.
+     * @default ''
+     */
     overline?: string;
+
+    /**
+     * The subtext of the dropdown menu.
+     * @default ''
+     */
     subtext?: string;
+
+    /**
+     * Custom header component for the dropdown menu.
+     * - If `customHeader` is provided, `showHeader`, `title`, `overline`, and `subtext` will be ignored.
+     * @default null
+     */
     customHeader?: React.ReactNode;
+
+    /**
+     * Whether the dropdown menu is searchable.
+     * @default false
+     */
+    searchable?: boolean;
+
+    /**
+     * Placeholder text for the search input.
+     * @default 'Search'
+     */
+    searchPlaceholder?: string;
+
+    /**
+     * Icon name for the search input.
+     * @default 'search'
+     */
+    searchIcon?: grauityIconName;
+
+    /**
+     * Callback function called when the search input value changes.
+     * @param value - The new value of the search input.
+     * @default A function that searches on label and description of items.
+     */
+    onSearchInputChange?: (value: string) => void;
+
+    /**
+     * Whether multiple items can be selected.
+     * @default false
+     */
+    multiple?: boolean;
+
+    /**
+     * List of items to be displayed in the dropdown menu.
+     * @default []
+     */
     items: BaseItemProps[];
-    showFooter?: boolean;
+
+    /**
+     * Whether to show action buttons (e.g., Apply, Clear All).
+     * @default false in single select mode, true in multiple select mode
+     */
+    showActionButtons?: boolean;
+
+    /**
+     * Whether to show the "Clear All" button.
+     * - If `showActionButtons` is false, `showClearAllButton` will be ignored.
+     * - If `multiple` is false, `showClearAllButton` will always be false.
+     * @default true
+     */
     showClearAllButton?: boolean;
-    showApplyButton?: boolean;
+
+    /**
+     * Text for the "Clear All" button.
+     * @default 'Clear All'
+     */
     clearAllButtonText?: string;
+
+    /**
+     * Text for the "Apply" button.
+     * @default 'Apply'
+     */
     applyButtonText?: string;
+
+    /**
+     * Callback function called when the "Clear All" button is clicked.
+     * @default Clears all selected items.
+     */
     onClearAll?: () => void;
-    onApply?: () => void;
-    onClose?: () => void;
+
+    /**
+     * Callback function called to apply the selected items.
+     * - In single select mode, if `showActionButtons` is false, `onApply` will be called when an option is clicked.
+     * - In multiple select mode, if `showActionButtons` is false, `onApply` will be called when clicked outside the dropdown menu.
+     * - If `showActionButtons` is true, `onApply` will be called when the "Apply" button is clicked.
+     * @param items - The selected items.
+     * @default null
+     */
+    onApply?: (items: BaseItemCheckboxProps[] | BaseItemDefaultProps) => void;
+
+    /**
+     * Callback function called when the dropdown menu is scrolled to the bottom.
+     * @default null
+     */
     onScrollToBottom?: () => void;
 }
