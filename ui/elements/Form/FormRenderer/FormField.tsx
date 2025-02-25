813d832f1b5d66c1926ad591e2a31bf83db8c8fa
Saksham Bindal <saksham.bindal2004@gmail.com>
2025-02-25 13:02:53 +0530

    fix renderer conditions

--- ui/elements/Form/FormRenderer/FormField.tsx
+++ ui/elements/Form/FormRenderer/FormField.tsx
@@ -14,28 +14,18 @@ const FormField = forwardRef<HTMLDivElement, FormFieldProps>((props, ref) => {
 
     if (typeof formField.renderer === 'function') {
         rendererComponent = <>{formField.renderer(props)}</>;
-    }
-
-    if (
+    } else if (
         formField.type === FormFieldType.TEXTFIELD ||
         formField.type === FormFieldType.DATE_PICKER
     ) {
         rendererComponent = <TextFieldWrapper {...props} />;
-    }
-
-    if (formField.type === FormFieldType.DROPDOWN) {
+    } else if (formField.type === FormFieldType.DROPDOWN) {
         rendererComponent = <DropdownWrapper {...props} />;
-    }
-
-    if (formField.type === FormFieldType.DROPDOWN_MENU) {
+    } else if (formField.type === FormFieldType.DROPDOWN_MENU) {
         rendererComponent = <DropdownMenuWrapper {...props} />;
-    }
-
-    if (formField.type === FormFieldType.ICON_BUTTON) {
+    } else if (formField.type === FormFieldType.ICON_BUTTON) {
         rendererComponent = <IconButton {...formField.rendererProps} />;
-    }
-
-    if (formField.type === FormFieldType.CHECKBOX) {
+    } else if (formField.type === FormFieldType.CHECKBOX) {
         rendererComponent = <CheckboxWrapper {...props} />;
     }
 
