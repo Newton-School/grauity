a84be5f82e606573c702deeced5dd07b53895e72
Saksham Bindal <saksham.bindal2004@gmail.com>
2025-03-03 16:22:52 +0530

    docstring for radio group component

--- ui/elements/Form/RadioButtonGroup/RadioButtonGroup.tsx
+++ ui/elements/Form/RadioButtonGroup/RadioButtonGroup.tsx
@@ -7,6 +7,31 @@ import RadioButton from '../RadioButton/RadioButton';
 import { StyledRadioButtonGroup } from './RadioButtonGroup.styles';
 import { RadioButtonGroupProps } from './types';
 
+/**
+ * RadioButtonGroup component is used to render a group of radio buttons.
+ * - It is used to select one option from a list of options.
+ * - It can be used to render a list of radio buttons with a label, help message, and error message.
+ *
+ * @example
+ * <RadioButtonGroup
+ *      name="radio-button-group"
+ *      label="Radio button group"
+ *      items={[
+ *          { value: 'option1', label: 'Option 1' },
+ *          { value: 'option2', label: 'Option 2' },
+ *          { value: 'option3', label: 'Option 3' },
+ *          { value: 'option4', label: 'Option 4', isDisabled: true },
+ *      ]}
+ *      value="option1"
+ *      onChange={(value) => {
+ *          console.log(value);
+ *      }}
+ *      isRequired={true}
+ *      helpMessage="This is a help message"
+ *      errorMessage="This is an error message"
+ *      className=""
+ * />
+ */
 const RadioButtonGroup = forwardRef<HTMLDivElement, RadioButtonGroupProps>(
     (props, ref) => {
         const {
