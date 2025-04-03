87922d6b953b7a046b96c4452dfe4187f38539bc
Mihir Sachdeva <mihirsachdeva.ms@gmail.com>
2025-04-03 12:08:48 +0530

    Add documentation for font tokens (#177)
    …

--- stories/atoms/CornerRadius/index.stories.tsx
+++ stories/atoms/CornerRadius/index.stories.tsx
@@ -69,6 +69,7 @@ export const CornerRadiusTokens = () => {
             borderAround={false}
             highlightHeaders={false}
             borderVertical={false}
+            hoverable
         />
     );
 };
