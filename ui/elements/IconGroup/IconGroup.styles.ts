b05ec0f6bc55d22ada45aa4eb311640aeb8e010e
Harshil <harshilvalecha@gmail.com>
2025-05-19 23:24:12 +0530

    Update ui/elements/IconGroup/IconGroup.styles.ts
    Co-authored-by: Mihir Sachdeva <mihirsachdeva.ms@gmail.com>

--- ui/elements/IconGroup/IconGroup.styles.ts
+++ ui/elements/IconGroup/IconGroup.styles.ts
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
 
 export const StyledIconGroup = styled.div<{
-    horizontal: boolean;
-    spacing: string;
+    $horizontal: boolean;
+    $spacing: string;
 }>`
   display: flex;
   flex-direction: \${props => (props.horizontal ? 'row' : 'column')};
