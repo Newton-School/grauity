4a96bb906189ade1cb9283d71519c2f84f53d9b2
Akash Gautam <108514708+akash3gtm@users.noreply.github.com>
2025-07-14 16:00:42 +0530

    Break motion.div and styled div into 2 different components for accordion (#254)
    ## Description
    
    - Provide a clear and concise description of your changes.
    - Try to use short paragraphs and bullet points to explain the changes.
    - The more understandable the description is, the more easy it makes for
    reviewers to merge this PR.
    - Include all the changes made explaining why it is needed.
    
    ## Type of Change
    
    - [x] 🐛 Bug fix (non-breaking change which fixes an issue)
    - [ ] ✨ New feature (non-breaking change which adds functionality)
    - [ ] 💥 Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    - [ ] 📚 Documentation update
    - [ ] 🎨 Style update (formatting, renaming)
    - [ ] ♻️ Code refactor (no functional changes)
    - [ ] ⚡️ Performance improvements
    - [ ] ✅ Test updates
    - [ ] 🔄 Other (please describe):
    
    ## Related Issues
    
    Link to any related issues using #issue-number:
    
    Fixes #
    
    ## Checklist
    
    - [ ] PR name uses present imperative tense and specifically describes
    the changes
      - Incorrect: ❌ Dependency version update
      - Correct: ✅ Update version of framer-motion
    - [ ] I have performed a self-review of my code
    - [ ] I have commented my code, particularly in hard-to-understand areas
    - [ ] I have made corresponding changes to the documentation
    - [ ] My changes generate no new TypeScript warnings
    - [ ] My changes does not hide eslint warnings un-necessarily
    - [ ] My pull request maintains linear history with the master branch to
    ensure that new changes are compatible with latest code.
    
    ## Additional Notes
    
    Add any additional notes for reviewers
    
    ## Screenshots (if applicable)
    
    Add screenshots to help explain your changes

--- ui/elements/Accordion/Accordion.styles.ts
+++ ui/elements/Accordion/Accordion.styles.ts
@@ -1,4 +1,3 @@
-import { motion } from 'framer-motion';
 import styled from 'styled-components';
 
 import { StyledDivProps } from '../../../common/types';
@@ -28,9 +27,7 @@ export const StyledAccordionHeaderSuffix = styled.div`
     align-items: center;
 `;
 
-export const StyledAccordionContent = styled(
-    motion.div
-)<StyledAccordionContentProps>`
+export const StyledAccordionContent = styled.div<StyledAccordionContentProps>`
     overflow: hidden;
     background-color: ${(props) => props.$contentBackgroundColor};
     padding: 0px 8px;
